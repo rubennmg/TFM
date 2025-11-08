fb1850c74e559ecaeb5ac9eec1c182f30150f5bd
rubennmg <rubenmg2015@gmail.com>
2025-11-08 12:17:57 +0100

    some typo

--- examples/code/exp_read_raw.py
+++ examples/code/exp_read_raw.py
@@ -1,27 +1,30 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
+from numpy.typing import NDArray
 
 # Define image dimensions
-width = 4096
-height = 2168
+width: int= 4096
+height: int = 2168
 
 # Load raw data
 with open('../raw_images/Image__2025-05-12__10-47-32.raw', 'rb') as f:
-    raw_data = np.fromfile(f, dtype=np.uint16)
+    raw_data: NDArray[np.uint16] = np.fromfile(f, dtype=np.uint16)
 
 # Reshape the data to 2D image
-image = raw_data.reshape((height, width))
+image: NDArray[np.uint16] = raw_data.reshape((height, width))
 
 # Display the image
-plt.imshow(image, cmap='gray')
-plt.title('16-bit Raw Image')
-plt.colorbar()
-plt.show()
+def show_image(image: NDArray[np.uint16]) -> None:
+    plt.imshow(image, cmap='gray')
+    plt.title('16-bit Raw Image')
+    plt.colorbar()
+    plt.show()
 
+show_image(image)
 #%%
 import enum
+import torch
 
 class Layout(enum.Enum):
     """Possible Bayer color filter array layouts.
@@ -132,8 +135,8 @@ class Debayer5x5(torch.nn.Module):
             self.index.repeat(
                 1,
                 1,
-                torch.div(H, 2, rounding_mode="floor"),
-                torch.div(W, 2, rounding_mode="floor"),
+                int(torch.div(H, 2, rounding_mode="floor").item()),
+                int(torch.div(W, 2, rounding_mode="floor").item()),
             ).expand(
                 B, -1, -1, -1
             ),  # expand for singleton batch dimension is faster
@@ -172,7 +175,7 @@ class Debayer5x5(torch.nn.Module):
             Layout.GRBG: torch.roll(rggb, 1, -1),
             Layout.GBRG: torch.roll(rggb, 1, -2),
             Layout.BGGR: torch.roll(rggb, (1, 1), (-1, -2)),
-        }.get(layout)
+        }.get(layout, rggb)
 
 debayer_processor = Debayer5x5(layout=Layout.RGGB).cuda()
 
--- examples/code/plt_read.py
+++ examples/code/plt_read.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.typing import NDArray
 
 # Define image dimensions
-width = 4096
-height = 2168
+width: int= 4096
+height: int = 2168
 
 # Load raw data
 with open('../raw_images/Image__2025-05-12__10-47-32.raw', 'rb') as f:
-    raw_data = np.fromfile(f, dtype=np.uint16)
+    raw_data: NDArray[np.uint16] = np.fromfile(f, dtype=np.uint16)
 
 # Reshape the data to 2D image
-image = raw_data.reshape((height, width))
+image: NDArray[np.uint16] = raw_data.reshape((height, width))
 
 # Display the image
-plt.imshow(image, cmap='gray')
-plt.title('16-bit Raw Image')
-plt.colorbar()
-plt.show()
+def show_image(image: NDArray[np.uint16]) -> None:
+    plt.imshow(image, cmap='gray')
+    plt.title('16-bit Raw Image')
+    plt.colorbar()
+    plt.show()
+
+show_image(image)
